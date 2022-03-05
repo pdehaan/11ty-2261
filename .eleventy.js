44f52ee0e65d2575fbe2a44e8c44e236be1a243e
Peter deHaan <peter@deseloper.com>
2022-03-05 07:22:04 -0800

    cleanup

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,7 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addShortcode("user", firstName => firstName);
   eleventyConfig.addPairedShortcode("user2", (content, firstName) => {
-      return `${content.trim()} ${firstName}`.trim();
+      return `${content.trim()} ${firstName}`;
   });
 
   return {
